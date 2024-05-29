42eebfbc7525797840656a953cb836e86f194673
Hou Rui <hourui@realtek-sg.com>
2024-05-29 11:17:07 +0800

    properly detect supported devices

--- EventListener.py
+++ EventListener.py
@@ -2,16 +2,16 @@ import asyncio
 import os
 
 import evdev
+from evdev.ecodes import ecodes
 from PySide6.QtCore import Property, QCoreApplication, QObject, Signal, Slot
 from PySide6.QtQml import QmlElement
-from PySide6.QtAsyncio import QAsyncioEventLoop
 from qasync import QEventLoop
 
 QML_IMPORT_NAME = "keylistener.backend"
 QML_IMPORT_MAJOR_VERSION = 1
 
-EV_KEY = evdev.ecodes.ecodes["EV_KEY"]
-KEY_ENTER = evdev.ecodes.ecodes["KEY_ENTER"]
+EV_KEY = ecodes["EV_KEY"]
+KEY_ENTER = ecodes["KEY_ENTER"]
 EVDEV_PATH = "/dev/input"
 
 
@@ -27,20 +27,20 @@ class EventListener(QObject):
         self.devices: list[evdev.InputDevice] = []
         self.tasks: list[asyncio.Task] = []
 
-    def isDeviceKeyboard(self, device: evdev.InputDevice) -> bool:
+    def supportsKeys(self, device: evdev.InputDevice, keys: list[str]) -> bool:
         caps = device.capabilities()
-        if keys := caps.get(EV_KEY):
-            return KEY_ENTER in keys
+        if supported := caps.get(EV_KEY):
+            return all(ecodes.get(key) in supported for key in keys)
         return False
 
-    def initDevices(self) -> list[evdev.InputDevice]:
+    def initDevices(self, supported: list[str]) -> list[evdev.InputDevice]:
         result = []
         for p in os.listdir(EVDEV_PATH):
             if not p.startswith("event"):
                 continue
             path = os.path.join(EVDEV_PATH, p)
             device = evdev.InputDevice(path)
-            if self.isDeviceKeyboard(device):
+            if self.supportsKeys(device, supported):
                 result.append(device)
         return result
 
@@ -72,7 +72,7 @@ class EventListener(QObject):
     def start(self, keys: list[str]):
         if self.isRunning:
             return
-        self.devices = self.initDevices()
+        self.devices = self.initDevices(keys)
         for device in self.devices:
             task = asyncio.ensure_future(self.listenAsync(device, keys))
             self.tasks.append(task)
