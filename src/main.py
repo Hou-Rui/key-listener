4fda6cc4465f16fb872df957755f98847f7043d8
Hou Rui <houruinus@gmail.com>
2024-07-25 20:56:10 +0800

    Fix missing icon in Wayland

--- src/main.py
+++ src/main.py
@@ -3,9 +3,11 @@ import sys
 from PySide6.QtWidgets import QApplication
 
 from window import MainWindow
+from utils import PROJECT_CODE_NAME
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setDesktopFileName(PROJECT_CODE_NAME)
     win = MainWindow()
     win.show()
     exit(app.exec())
